d512c89b6c3ad4569828ea41a1536191d0990c49
ST3603 <ST3603@ST3603>
2024-02-02 15:42:40 +0600

    /commit

--- Wordlee/Views/Pages/GamePage.xaml.cs
+++ Wordlee/Views/Pages/GamePage.xaml.cs
@@ -22,14 +22,83 @@ namespace Wordlee.Views.Pages
     /// </summary>
     public partial class GamePage : Page
     {
+        int row = 0;
+        WordViewModel _vm;
+
         public GamePage(WordViewModel vm)
         {
             InitializeComponent();
             DataContext = vm;
+            _vm = vm;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) => MyFrame.Exit();
 
         private void ButtonBack_Click(object sender, RoutedEventArgs e) => MyFrame.frame.GoBack();
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            switch (row)
+            {
+                case 0:
+                    {
+                        tb00.Text = tb.Text[0].ToString();
+                        tb01.Text = tb.Text[1].ToString();
+                        tb02.Text = tb.Text[2].ToString();
+                        tb03.Text = tb.Text[3].ToString();
+                        tb04.Text = tb.Text[4].ToString();
+                        break;
+                    }
+                case 1:
+                    {
+                        tb10.Text = tb.Text[0].ToString();
+                        tb11.Text = tb.Text[1].ToString();
+                        tb12.Text = tb.Text[2].ToString();
+                        tb13.Text = tb.Text[3].ToString();
+                        tb14.Text = tb.Text[4].ToString();
+                        break;
+                    }
+                case 2:
+                    {
+                        tb20.Text = tb.Text[0].ToString();
+                        tb21.Text = tb.Text[1].ToString();
+                        tb22.Text = tb.Text[2].ToString();
+                        tb23.Text = tb.Text[3].ToString();
+                        tb24.Text = tb.Text[4].ToString();
+                        break;
+                    }
+                case 3:
+                    {
+                        tb30.Text = tb.Text[0].ToString();
+                        tb31.Text = tb.Text[1].ToString();
+                        tb32.Text = tb.Text[2].ToString();
+                        tb33.Text = tb.Text[3].ToString();
+                        tb34.Text = tb.Text[4].ToString();
+                        break;
+                    }
+                case 4:
+                    {
+                        tb40.Text = tb.Text[0].ToString();
+                        tb41.Text = tb.Text[1].ToString();
+                        tb42.Text = tb.Text[2].ToString();
+                        tb43.Text = tb.Text[3].ToString();
+                        tb44.Text = tb.Text[4].ToString();
+                        break;
+                    }
+            }
+        }
+
+        private int Contains(char c, int index)
+        {
+            if (!_vm.SelectedWord.Contains(c))
+                return 0;
+            else
+            {
+                if (_vm.SelectedWord.)
+                    return 2;
+                return 1;
+            }
+        }
     }
 }
